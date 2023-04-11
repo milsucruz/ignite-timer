e3b2f071c12ca6239f24b5e79144798cf3122287
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-04-11 15:12:43 -0300

    Global Styles

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,8 +17,13 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  background-color: ${props => props.theme.primary};
+  margin: 8px;
+  border-radius: 8px;
+  border: 0;
+  
 
+  background-color: ${props => props.theme["green-500"]};
+  color: ${props => props.theme.white};
   /* ${props => {
     return css`background-color: ${buttonVariants[props.variant]}`
   }} */
