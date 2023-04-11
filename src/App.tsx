c074a332ba09655db0029145ae7ed0e7ce5e7649
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-04-11 10:31:42 -0300

    Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
 
   return (
-    <div>
-      <h1>
-        hello world
-      </h1>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="sucess" />
+      <Button variant="danger" />
+      <Button/>
+    </ThemeProvider>
   )
 }
