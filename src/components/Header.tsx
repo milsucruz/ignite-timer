a7842b9d24c46a29bda4d487a590a8b1b1610ecc
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-04-12 10:34:32 -0300

    Default Layout (index && styles)

--- src/components/Header.tsx
+++
@@ -1,3 +0,0 @@
-export function Header() {
-  return <h1>HEADER</h1>
-}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+
+      <nav>
+        <a href="#">timer</a>
+        <a href="#">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
