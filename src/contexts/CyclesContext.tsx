b0efd5f117e8be577d1e125752284a6ddfc0ba9f
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-04-18 15:20:34 -0300

    Reducer fix

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -45,13 +45,15 @@ export function CyclesContextProvider({
       cycles: [],
       activeCycleId: null,
     },
-    () => {
+    (initialState) => {
       const storedStateAsJSON = localStorage.getItem(
         '@ignite-timer:cycles-state-1.0.0',
       )
       if (storedStateAsJSON) {
         return JSON.parse(storedStateAsJSON)
       }
+
+      return initialState
     },
   )
 
